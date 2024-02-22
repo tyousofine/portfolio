b4af29c6c7f098e8d298468df101f82f25875892
tyousofine <tyousofine@gmail.com>
2024-02-22 15:20:15 -0500

    switch pooject order

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,39 +3,40 @@ import plugStreaming from '../images/PlugStreaming.png';
 import todoApp from '../images/todoApp.png';
 import portfolio from '../images/portfolio.png';
 
-export const projects = [{
-    image: onTrack,
-    title: 'OnTrack',
-    stack: ['React', 'HTML', 'SCSS', 'Firebase'],
-    description: 'A simple project and task managing desktop app, designed and effective for small to medium sized teams. App comes with authentication, data management, as well as a live chat and comments feature.',
-    webLink: "https://ontrackk.vercel.app/",
-    codeLink: "https://github.com/tyousofine/onTrack"
-},
-{
-    image: plugStreaming,
-    title: 'Library and Dashboard for Plug Streaming',
-    stack: ['JS', 'D3', 'HTML', 'CSS'],
-    description: 'A library and Dashboard for a streaming service Called Plug Streaming developed according to their specifications. Dashboard uses Youtube statistics to provide visualized analysis of data for the stake holders reference.',
-    webLink: "https://plugstreaming.vercel.app",
-    codeLink: "https://github.com/tyousofine/PStreaming"
-},
-{
-    image: todoApp,
-    title: "TY's Todo App",
-    stack: ['JS', 'React Native', 'SQLite'],
-    description: "Who hasn't done a todo App in their coding journey? And here is a mine. A simple todo app for Android devices, published in Google Playstore. App is built with React Native and uses SQLite and the device's local storage.",
-    expoSnack: "https://snack.expo.dev/@tyousofine/tystodoapp?platform=android",
-    googlePlay: "http://play.google.com/store/apps/details?id=com.ty.creations.todo.app"
-},
+export const projects = [
+    {
+        image: plugStreaming,
+        title: 'Library and Dashboard for Plug Streaming',
+        stack: ['JS', 'D3', 'HTML', 'CSS'],
+        description: 'A library and Dashboard for a streaming service Called Plug Streaming developed according to their specifications. Dashboard uses Youtube statistics to provide visualized analysis of data for the stake holders reference.',
+        webLink: "https://plugstreaming.vercel.app",
+        codeLink: "https://github.com/tyousofine/PStreaming"
+    },
+    {
+        image: todoApp,
+        title: "<TY/>'s Todo App",
+        stack: ['JS', 'React Native', 'SQLite'],
+        description: "Who hasn't done a todo App in their coding journey? And here is a mine. A simple todo app for Android devices, published in Google Playstore. App is built with React Native and uses SQLite and the device's local storage.",
+        expoSnack: "https://snack.expo.dev/@tyousofine/tystodoapp?platform=android",
+        googlePlay: "http://play.google.com/store/apps/details?id=com.ty.creations.todo.app"
+    },
+    {
+        image: onTrack,
+        title: 'OnTrack',
+        stack: ['React', 'HTML', 'SCSS', 'Firebase'],
+        description: 'A simple project and task managing desktop app, designed and effective for small to medium sized teams. App comes with authentication, data management, as well as a live chat and comments feature.',
+        webLink: "https://ontrackk.vercel.app/",
+        codeLink: "https://github.com/tyousofine/onTrack"
+    },
 
-{
-    image: portfolio,
-    title: "<TY/> Portfolio",
-    stack: ['JS', 'React', 'MatterJS'],
-    description: "Personal portfolio website designed and built by this page owner, using react, and Matter.js for the Hero Animation. ",
-    webLink: "https://tyoussefian.vercel.app",
-    codeLink: "https://github.com/tyousofine/portfolio"
-},
+    {
+        image: portfolio,
+        title: "<TY/> Portfolio",
+        stack: ['JS', 'React', 'MatterJS'],
+        description: "Personal portfolio website designed and built by this page owner, using react, and Matter.js for the Hero Animation. ",
+        webLink: "https://tyoussefian.vercel.app",
+        codeLink: "https://github.com/tyousofine/portfolio"
+    },
 
 
 
